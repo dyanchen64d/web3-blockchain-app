c684cdd400f89e1dbcb7d8db37b1d20a18d75077
chen.zhang <zhangchen64d@gmail.com>
2023-02-16 15:24:25 +0900

    update

--- client/src/utils/const.js
+++ client/src/utils/const.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x817D5252Cb1c248F315b752195726F4783084fFA';
+export const contractAddress = '0xF3843F82d94420bDD5ECd6CA3983D6648e5F592E';
